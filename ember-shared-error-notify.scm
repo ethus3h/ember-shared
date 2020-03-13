43d7aed3f184a15c67f052276cab7c7e80317e1c
ethus3h <kolubat@gmail.com>
2020-03-13 00:31:33 -0400

    Update Git repository using egup-single

--- ember-shared-error-notify.scm
+++ ember-shared-error-notify.scm
@@ -1,15 +1,15 @@
 (define-public ember-shared-error-notify
   (package
     (name "ember-shared-error-notify")
-    (version "0-b1175e54fb2a533bfffac54f80cc7d665ce1a09b")
+    (version "0-583d03aef1f34a2ffad6a09d82292501585d9e5e")
     (source (origin
               (method git-fetch)
               (uri (git-reference
                 (url "https://github.com/ethus3h/ember-shared.git")
-                (commit "b1175e54fb2a533bfffac54f80cc7d665ce1a09b")))
+                (commit "583d03aef1f34a2ffad6a09d82292501585d9e5e")))
               (sha256
                (base32
-                "0hbfkc3c18qbnpkkp5anvk60ykfz76vms6x1a5mjqx1xfa52fw11"))))
+                "0p2sdxjamgq0c2pslqjgb90v25q8ky26l8vckrzii2dwywjsidrp"))))
     (build-system gnu-build-system)
     (arguments '(#:configure-flags '("--module" "error-notify") #:phases (modify-phases %standard-phases (delete 'check))))
     (inputs `(("xxd" ,xxd)))
