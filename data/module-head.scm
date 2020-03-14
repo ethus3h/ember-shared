fa9835aa62ed06c260fa115ae89615f05c7bc838
ethus3h <kolubat@gmail.com>
2020-03-14 00:54:19 -0400

    Update Git repository using egup-single

--- data/module-head.scm
+++ data/module-head.scm
@@ -18,3 +18,22 @@
   #:use-module (guix licenses)
   #:use-module (gnu packages vim))
 
+(define-public TEMPLATE-PLACEHOLDER-PACKAGENAME
+  (package
+    (name "TEMPLATE-PLACEHOLDER-PACKAGENAME")
+    (version "TEMPLATE-PLACEHOLDER-VERSION")
+    (source (origin
+              (method git-fetch)
+              (uri (git-reference
+                (url "https://github.com/ethus3h/ember-shared.git")
+                (commit "TEMPLATE-PLACEHOLDER-COMMIT")))
+              (sha256
+               (base32
+                "TEMPLATE-PLACEHOLDER-HASH"))))
+    (build-system gnu-build-system)
+    (arguments '(#:configure-flags '("--module" "error-notify") #:phases (modify-phases %standard-phases (delete 'check))))
+    (inputs `(("xxd" ,xxd)))
+    (synopsis "ember-shared error-notify script")
+    (description "Shell script to notify of errors")
+    (home-page "http://futuramerlin.com/ancillary/ember-shared/")
+    (license agpl3+)))
