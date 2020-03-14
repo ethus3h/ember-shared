30ee2c9f806ea24a1ba2adec4a3a24558fa7c3b3
ethus3h <kolubat@gmail.com>
2020-03-14 03:19:08 -0400

    Update Git repository using egup-single

--- data/packages/dce-in.scm
+++ data/packages/dce-in.scm
@@ -21,6 +21,7 @@
   #:use-module (gnu packages perl)
   #:use-module (gnu packages nano)
   #:use-module (gnu packages admin)
+  #:use-module (gnu packages admin)
 )
 
 (define-public ember-shared-error-notify
