b0518940608bec9d75cfbd3f039b23281816d352
ethus3h <kolubat@gmail.com>
2020-03-14 03:17:50 -0400

    Update Git repository using egup-single

--- data/packages/dce-in.scm
+++ data/packages/dce-in.scm
@@ -113,7 +113,9 @@
         ; inputs this needs are implicit in the build system: grep, findutils
         ; csum (depends hashdeep), ember_bash_setup_extra (see for dependencies), ember-shared-update, findq/findq-update (depend locate), htmlToText.pl (depends HTML::Restrict), serve-ember-web-site (depends PHP), wave2png.py (depends wave, matplotlib, pylab). ia, wget, rsync, sshpass, csvfix, ssh, jq, grab-site, phantomjs, ldconfig, python, ffmpeg, youtube-dl, git, sqlite3, moreutils, imagemagick, xz
         ;("hashdeep" ,hashdeep)
+        ; Miscellaneous scripts' dependencies:
         ("glibc" ,glibc) ; available but not propagated by default, I think?? Not sure how to tell for sure. For ldconfig
+        ; Main ember_bash_setup_extra dependencies:
         ("sudo" ,sudo)
     ))
     (synopsis "ember-shared core")
