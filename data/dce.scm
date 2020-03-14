f9ec46f114a047e997f9482396276dc40cae6230
ethus3h <kolubat@gmail.com>
2020-03-14 01:07:32 -0400

    Update Git repository using egup-single

---
+++ data/dce.scm
@@ -0,0 +1,39 @@
+;;; GNU Guix is free software; you can redistribute it and/or modify it
+;;; under the terms of the GNU General Public License as published by
+;;; the Free Software Foundation; either version 3 of the License, or (at
+;;; your option) any later version.
+;;;
+;;; GNU Guix is distributed in the hope that it will be useful, but
+;;; WITHOUT ANY WARRANTY; without even the implied warranty of
+;;; MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+;;; GNU General Public License for more details.
+;;;
+;;; You should have received a copy of the GNU General Public License
+;;; along with GNU Guix.  If not, see <http://www.gnu.org/licenses/>.
+
+(define-module (gnu packages dce)
+  #:use-module (guix packages)
+  #:use-module (guix git-download)
+  #:use-module (guix build-system gnu)
+  #:use-module (guix licenses)
+  #:use-module (gnu packages vim))
+
+(define-public ember-shared-error-notify
+  (package
+    (name "ember-shared-error-notify")
+    (version "1.1.4.475-a7b3025f99862b338617130c98985e6a5fbcfe3a")
+    (source (origin
+              (method git-fetch)
+              (uri (git-reference
+                (url "https://github.com/ethus3h/ember-shared.git")
+                (commit "a7b3025f99862b338617130c98985e6a5fbcfe3a")))
+              (sha256
+               (base32
+                "0d930wrgi1110rs0y7vyvc8vpd7lnygkyrndwzv0vypzlqnwgcgy"))))
+    (build-system gnu-build-system)
+    (arguments '(#:configure-flags '("--module" "error-notify") #:phases (modify-phases %standard-phases (delete 'check))))
+    (inputs `(("xxd" ,xxd)))
+    (synopsis "ember-shared error-notify script")
+    (description "Shell script to notify of errors")
+    (home-page "http://futuramerlin.com/ancillary/ember-shared/")
+    (license agpl3+)))
