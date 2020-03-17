c038a4bf5442626c8e3dd188b63298b36ff00bae
ethus3h <kolubat@gmail.com>
2020-03-16 21:31:10 -0400

    Update Git repository using egup-single

--- data/packages/dce-in.scm
+++ data/packages/dce-in.scm
@@ -183,7 +183,7 @@
             )
         ))
     ))
-    (propagated-inputs `(
+    (inputs `(
         ("ember-shared-error-notify" ,ember-shared-error-notify)
         ; many inputs this needs are implicit in the build system: bash, coreutils, sed, gawk, diffutils
         ("util-linux" ,util-linux)
