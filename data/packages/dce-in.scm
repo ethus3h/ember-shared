6374a0c015e224fb092a02d6b868b1f62733d6d1
ethus3h <kolubat@gmail.com>
2020-03-16 22:43:19 -0400

    Update Git repository using egup-single

--- data/packages/dce-in.scm
+++ data/packages/dce-in.scm
@@ -188,7 +188,7 @@
                         substitute* "bootstrap.sh" (("/bin/rm") "rm")
                     )
                     (
-                        substitute* "bootstrap.sh" (("/bin/rm") "rm")
+                        substitute* "Makefile.am" (("/bin/rm") "rm")
                     )
                     #t
               ))
