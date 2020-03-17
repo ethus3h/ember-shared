cc3bc10a8dc0ba76212a3ac683c34104181a96de
ethus3h <kolubat@gmail.com>
2020-03-16 21:45:38 -0400

    Update Git repository using egup-single

--- data/packages/dce-in.scm
+++ data/packages/dce-in.scm
@@ -175,7 +175,7 @@
               (sha256
                (base32
                 "1g3xyrr2gjb6h511m0wwjhk5s1v5wz16nd377ykscnsfwwrmks6m"))
-              (snippet (lambda _ ))
+              (snippet (lambda _ (invoke "/bin/rm" "-r" "dist")))
             ))
     (build-system gnu-build-system)
     (arguments '(
