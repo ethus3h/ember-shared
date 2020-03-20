b6a13bf8cd72c89b5be9926d50a9f18b8e760fa4
ethus3h <kolubat@gmail.com>
2020-03-20 19:25:41 -0400

    Update Git repository using egup-single

--- data/packages/dce-in.scm
+++ data/packages/dce-in.scm
@@ -180,7 +180,7 @@
     (build-system gnu-build-system)
     (propagated-inputs `(
         ("ember-shared-extra" ,ember-shared-extra)
-        ;("futuramerlin-web-toolkit" ,ember-shared-extra)
+        ;("futuramerlin-web-toolkit" ,futuramerlin-web-toolkit)
     ))
     (synopsis "Collection of tools for working with files and filesystems")
     (description "Collection of tools for working with files and filesystems")
