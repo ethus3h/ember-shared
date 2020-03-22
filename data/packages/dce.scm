9e4383fa1c744cb7d945e6114a815f223708a96c
ethus3h <kolubat@gmail.com>
2020-03-21 20:29:16 -0400

    Update Git repository using egup-single

--- data/packages/dce.scm
+++ data/packages/dce.scm
@@ -98,15 +98,15 @@
 (define-public ember-shared-main
   (package
     (name "ember-shared-main")
-    (version "1.1.4.514-5786ca3e12be14badbc00cac0239138edf865a77")
+    (version "1.1.4.515-dae646deda98224a0e6248900e1008b772445048")
     (source (origin
               (method git-fetch)
               (uri (git-reference
                 (url "https://github.com/ethus3h/ember-shared.git")
-                (commit "5786ca3e12be14badbc00cac0239138edf865a77")))
+                (commit "dae646deda98224a0e6248900e1008b772445048")))
               (sha256
                (base32
-                "1nd1gai3w0d1n5s6z26vvhx019c51gfgnp2xan66da76cvqscxfn"))))
+                "0mgh5614hym6izmh5ydvx7zwi02q7gjcf0zp43ccb45qgp3bbylp"))))
     (build-system gnu-build-system)
     (arguments '(#:configure-flags '("--module=main") #:phases (modify-phases %standard-phases (delete 'check))))
     (propagated-inputs `(
