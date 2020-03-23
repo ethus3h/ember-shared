b06eb1a9ac98576aadb041d7cd197321492cd70e
ethus3h <kolubat@gmail.com>
2020-03-23 19:41:06 -0400

    Update Git repository using egup-single

--- data/packages/dce.scm
+++ data/packages/dce.scm
@@ -212,6 +212,7 @@
                 (sha256
                 (base32
                     "1a1jc6x4m14s7s7xbhzp6qspk6wsdx7vraggcpxrgiqhslszb6rh"))
+                (modules '((guix build utils)))
                 (snippet '(begin
                     (copy-recursively '((assoc-ref inputs "dce-input-ucd") build-temp/unpacked/))
                     (touch build-temp/dist-already-unpacked)
@@ -269,6 +270,7 @@
               (modules '((guix build utils)))
               (snippet '(begin
                     (for-each delete-file-recursively '("dist" "tests"))
+                    (invoke "ls" "tests")
                     (substitute* "bootstrap.sh" (("/bin/rm") "rm"))
                     (substitute* "Makefile.am"
                         (("src tests man tests/testfiles") "src man")
