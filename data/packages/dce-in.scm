8b26db181bf9c3d2a65f3928b0f7995f7ef84a65
ethus3h <kolubat@gmail.com>
2020-03-23 19:56:29 -0400

    Update Git repository using egup-single

--- data/packages/dce-in.scm
+++ data/packages/dce-in.scm
@@ -270,7 +270,7 @@
               (modules '((guix build utils)))
               (snippet '(begin
                     (for-each delete-file-recursively '("dist" "tests"))
-                    (invoke "ls")
+                    (invoke "bash" "./bootstrap.sh")
                     (substitute* "bootstrap.sh" (("/bin/rm") "rm"))
                     (substitute* "Makefile.am"
                         (("src tests man tests/testfiles") "src man")
