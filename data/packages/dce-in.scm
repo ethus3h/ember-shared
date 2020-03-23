882e8b41af5cef1f62c09aef436ad1d02db6e41a
ethus3h <kolubat@gmail.com>
2020-03-23 19:57:43 -0400

    Update Git repository using egup-single

--- data/packages/dce-in.scm
+++ data/packages/dce-in.scm
@@ -269,8 +269,7 @@
               ; Also remove the test suite, since it depends on a pre-built known good binary to test against
               (modules '((guix build utils)))
               (snippet '(begin
-                    (for-each delete-file-recursively '("dist" "tests"))
-                    (invoke "bash" "./bootstrap.sh")
+                    (for-each delete-file-recursively '("dist" "testsa"))
                     (substitute* "bootstrap.sh" (("/bin/rm") "rm"))
                     (substitute* "Makefile.am"
                         (("src tests man tests/testfiles") "src man")
