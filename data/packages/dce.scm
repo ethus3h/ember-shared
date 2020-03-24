6071a9b5ac1e3a256ac96df97f35b27e562937a7
ethus3h <kolubat@gmail.com>
2020-03-23 21:55:03 -0400

    Update Git repository using egup-single

--- data/packages/dce.scm
+++ data/packages/dce.scm
@@ -201,6 +201,7 @@
     (license (list agpl3+ bsd-2))))
 
 (define dce-input-ucd
+    (let ((version "12.0.0"))
     (origin
               (method url-fetch)
               (uri (string-append "https://www.unicode.org/Public/" version "/ucdxml/ucd.all.flat.zip"))
@@ -209,7 +210,7 @@
                (base32
                 "18nmj93m71jl399bzzdlprz8w7idcmbg71x3fz0lpj62sl0jhpnq"))
         )
-) ;  returns a derivation object(?)
+)) ;  returns a derivation object(?)
 
 (define-public dce
   (package
