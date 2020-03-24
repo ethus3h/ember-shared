d6f5968057d0854cb03fe18c24598d459adbbf86
ethus3h <kolubat@gmail.com>
2020-03-23 22:50:25 -0400

    Update Git repository using egup-single

--- data/packages/dce.scm
+++ data/packages/dce.scm
@@ -241,7 +241,6 @@
                 (snippet #~(begin
                     (lambda*
                         (#:key inputs outputs #:allow-other-keys)
-                        (modules '((guix build utils)))
                         (let
                             (
                                 (dce-input-ucd (assoc-ref inputs "dce-input-ucd"))
@@ -252,8 +251,9 @@
                     )
                     ;(copy-file (assoc-ref inputs "dce-input-ucd") "build-temp/distfiles/")
                     ;(copy-recursively (assoc-ref inputs "dce-input-ucd") "build-temp/distfiles/")
+                    (lambda*
                     (invoke "bash" "./support/build-scripts/dist-unpack")
-                    (invoke "touch" "build-temp/dist-already-unpacked")
+                    (invoke "touch" "build-temp/dist-already-unpacked"))
                     #t
                 ))
         )
