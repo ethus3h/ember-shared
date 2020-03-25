a12b86ad6653f7a4aee7cda1069942a32889144a
ethus3h <kolubat@gmail.com>
2020-03-24 21:07:40 -0400

    Update Git repository using egup-single

--- data/packages/dce-in.scm
+++ data/packages/dce-in.scm
@@ -252,6 +252,7 @@
             add-after 'unpack 'prepare-additional
                 (lambda* (#:key inputs #:allow-other-keys)
                     (mkdir-p "build-temp/distfiles/")
+                    ;(copy-file (assoc-ref inputs "dce-input-ucd") "build-temp/distfiles/")
                     (invoke "cp" "-v" (assoc-ref inputs "dce-input-ucd") "build-temp/distfiles/")
                     (invoke "bash" "./support/build-scripts/dist-unpack")
                     (invoke "touch" "build-temp/dist-already-unpacked")
