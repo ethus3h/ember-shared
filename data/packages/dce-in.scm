8808009045020bb901d55171726d77bc60992dd8
ethus3h <kolubat@gmail.com>
2020-03-24 22:18:10 -0400

    Update Git repository using egup-single

--- data/packages/dce-in.scm
+++ data/packages/dce-in.scm
@@ -229,7 +229,7 @@
                 (modules '((guix build utils)))
                 (snippet '(begin
                     (for-each delete-file-recursively '(".egup.stat" ".stagel-cache" "built"))
-                    (for-each delete-file-recursively (for-each find-files (for-each find-files (find-files "tests/stagel")) "run") ; "run" folders hold the generated output, while "out" folders hold the expected output
+                    (for-each delete-file (for-each find-files (for-each find-files (find-files "tests/stagel")) "run") ; "run" folders hold the generated output, while "out" folders hold the expected output
                     #t
                 ))
         )
