853abccb8ab1cfdacbdafe5efce2913b50fb80e5
ethus3h <kolubat@gmail.com>
2020-03-25 21:04:47 -0400

    Update Git repository using egup-single

--- data/packages/dce-in.scm
+++ data/packages/dce-in.scm
@@ -319,7 +319,7 @@
 ; DCE distfiles
 
 (define dce-input-ucd
-    ; This is a hidden package that is used as an input to the main dce package. It just returns the ZIP file. To get the source for dce including this package, use "guix build --sources=all dce".
+    ; This is a hidden input that is used as an input to the main dce package. It just returns the ZIP file. To get the source for dce including this package, use "guix build --sources=all dce".
     (let ((version "12.0.0"))
     (origin
               (method url-fetch)
