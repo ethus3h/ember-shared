6370374e629bc97938be8f94258fbc500522502b
ethus3h <kolubat@gmail.com>
2020-03-25 21:07:27 -0400

    Update Git repository using egup-single

--- data/packages/dce-in.scm
+++ data/packages/dce-in.scm
@@ -214,7 +214,7 @@
     (home-page "http://futuramerlin.com/ancillary/crystallize/")
     (license (list agpl3+ bsd-2))))
 
-(define dce-common-attributes
+(define-public dce-common-attributes
   (package
     (name "dce")
     (version "TEMPLATE-PLACEHOLDER-VERSION:dce")
