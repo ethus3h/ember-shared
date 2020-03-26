7565205b8f30459c5039ceea82a76dccb7660c5c
ethus3h <kolubat@gmail.com>
2020-03-25 20:38:25 -0400

    Update Git repository using egup-single

--- data/packages/dce-in.scm
+++ data/packages/dce-in.scm
@@ -282,6 +282,7 @@
         )
     ))
     (inputs `(
+        ("dce-input-source" ,dce-input-source)
         ("dce-input-ucd" ,dce-input-ucd)
         ("unzip" ,unzip) ; to unpack distfiles
     ))
