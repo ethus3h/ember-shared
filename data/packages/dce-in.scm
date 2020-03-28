d0dabb81e8d789799d292089a149e8587d72c998
ethus3h <kolubat@gmail.com>
2020-03-28 18:48:40 -0400

    Update Git repository using egup-single

--- data/packages/dce-in.scm
+++ data/packages/dce-in.scm
@@ -319,9 +319,6 @@
             ))
             (inputs `(
                 ("dce-bootstrap" ,dce-dist)
-                ("dce-data" ,dce-data)
-                ("ember-shared-core" ,ember-shared-core)
-                ; can use srsync from crystallize to copy the built webextension
             ))
             (propagated-inputs `(
                 ("dce-bootstrap" ,dce-dist)
