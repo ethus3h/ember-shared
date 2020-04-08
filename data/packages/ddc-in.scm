ea67faa3b407cbf118beeea8aa6d9d6d5a0dfe23
ethus3h <kolubat@gmail.com>
2020-04-08 17:15:13 -0400

    Update Git repository using egup-single

--- data/packages/ddc-in.scm
+++ data/packages/ddc-in.scm
@@ -42,6 +42,7 @@
   #:use-module (gnu packages algebra)
   #:use-module (gnu packages autotools)
   #:use-module (gnu packages base)
+  #:use-module (gnu packages autotools)
   #:use-module (gnu packages bash)
   #:use-module (gnu packages compression)
   #:use-module (gnu packages imagemagick)
