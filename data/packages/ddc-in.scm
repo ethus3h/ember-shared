a69c381a3721419e2b35063208c9fe52209b70eb
Elli Chandler <elli@futuramerlin.com>
2020-07-30 23:40:25 -0400

    Update Git repository using egup-single

--- data/packages/ddc-in.scm
+++ data/packages/ddc-in.scm
@@ -224,42 +224,42 @@
     (license (list agpl3+ bsd-2))))
 
 (define-public ddc-common-attributes
-        (package
-            ; do-nothing package to hold common aspects of ddc packages
-            (name "ddc-common-attributes")
-            (version "TEMPLATE-PLACEHOLDER-VERSION:ddc-fast")
-            (build-system trivial-build-system)
-            (source (origin
-                (method git-fetch)
-                (uri (git-reference
-                    (url "https://github.com/ethus3h/ember-information-technology-environment.git")
-                    (commit "TEMPLATE-PLACEHOLDER-COMMIT:ddc")))
-                (sha256
-                (base32
-                    "TEMPLATE-PLACEHOLDER-HASH:ddc"))
-                (modules '((guix build utils)))
-                (snippet '(begin
-                    (for-each delete-file-recursively '(".egup.stat" ".stagel-cache" "built"))
-                    (for-each delete-file-recursively (find-files "tests" "^run$" #:directories? #t)) ; "run" folders hold the generated output, while "out" folders hold the expected output
-                    #t
-                ))
+    (package
+        ; do-nothing package to hold common aspects of ddc packages
+        (name "ddc-common-attributes")
+        (version "TEMPLATE-PLACEHOLDER-VERSION:ddc-fast")
+        (build-system trivial-build-system)
+        (source (origin
+            (method git-fetch)
+            (uri (git-reference
+                (url "https://github.com/ethus3h/ember-information-technology-environment.git")
+                (commit "TEMPLATE-PLACEHOLDER-COMMIT:ddc")))
+            (sha256
+            (base32
+                "TEMPLATE-PLACEHOLDER-HASH:ddc"))
+            (modules '((guix build utils)))
+            (snippet '(begin
+                (for-each delete-file-recursively '(".egup.stat" ".stagel-cache" "built"))
+                (for-each delete-file-recursively (find-files "tests" "^run$" #:directories? #t)) ; "run" folders hold the generated output, while "out" folders hold the expected output
+                #t
             ))
-            (synopsis "Deterministic Document Computing")
-            (description "Document-centric software environment")
-            (home-page "http://futuramerlin.com/specification/engineering-and-tech/information-technology/software/")
-            (license (list
-                agpl3+
-                unicode
-                silofl1.1 ; soccer.otf
-                ; FIXME: papaparse
-                (x11-style "file://thirdparty-licenses/LICENSE.base16b.md")
-                (x11-style "file://thirdparty-licenses/LICENSE.kde-syntax-highlighting.md")
-                (x11-style "file://thirdparty-licenses/LICENSE.wtf8.md")
-            ))
-            (propagated-inputs `(
-                ("ember-shared-core" ,ember-shared-core)
-            ))
-        )
+        ))
+        (synopsis "Deterministic Document Computing")
+        (description "Document-centric software environment")
+        (home-page "http://futuramerlin.com/specification/engineering-and-tech/information-technology/software/")
+        (license (list
+            agpl3+
+            unicode
+            silofl1.1 ; soccer.otf
+            ; FIXME: papaparse
+            (x11-style "file://thirdparty-licenses/LICENSE.base16b.md")
+            (x11-style "file://thirdparty-licenses/LICENSE.kde-syntax-highlighting.md")
+            (x11-style "file://thirdparty-licenses/LICENSE.wtf8.md")
+        ))
+        (propagated-inputs `(
+            ("ember-shared-core" ,ember-shared-core)
+        ))
+    )
 )
 
 (define-public ddc-common-attributes-slow
